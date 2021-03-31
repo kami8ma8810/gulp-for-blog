c9afb2939178d822344ede4b8848c677a3416021
Hayato Kamiyama <kami8ma8810@gmail.com>
2021-03-31 13:03:54 +0900

    npm list削除

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 //gulp.〇〇の処理は全部定数でまとめる
 const {
-  src,
-  dest,
-  watch,
-  lastRun,
-  series,
-  parallel
+	src,
+	dest,
+	watch,
+	lastRun,
+	series,
+	parallel
 } = require("gulp");
 
 // html
@@ -35,185 +35,185 @@ const browserSync = require("browser-sync").create();
 
 //パス設定
 const paths = {
-  html: {
-    src: "./src/*.html",
-    dist: "./dist/",
-  },
-  styles: {
-    src: "./src/scss/**/*.scss",
-    dist: "./dist/css/",
-    map: "./map",
-  },
-  scripts: {
-    src: "./src/js/**/*.js",
-    dist: "./dist/js/",
-  },
-  images: {
-    src: "./src/img/**/*.{jpg,jpeg,png,gif,svg}",
-    dist: "./dist/img/",
-  },
+	html: {
+		src: "./src/*.html",
+		dist: "./dist/",
+	},
+	styles: {
+		src: "./src/scss/**/*.scss",
+		dist: "./dist/css/",
+		map: "./map",
+	},
+	scripts: {
+		src: "./src/js/**/*.js",
+		dist: "./dist/js/",
+	},
+	images: {
+		src: "./src/img/**/*.{jpg,jpeg,png,gif,svg}",
+		dist: "./dist/img/",
+	},
 };
 
 // htmlフォーマット
 const htmlFormat = () => {
-  return src(paths.html.src)
-    .pipe(
-      plumber({
-        //エラーがあっても処理を止めない
-        errorHandler: notify.onError("Error: <%= error.message %>"),
-      })
-    )
-    .pipe(
-      htmlMin({
-        //HTMLの圧縮
-        removeComments: true, //コメントを削除
-        collapseWhitespace: true, //余白を詰める
-        preserveLineBreaks: true, //タグ間の改行を詰める
-        removeEmptyAttributes: false //空属性を削除しない
-      })
-    )
-    .pipe(dest(paths.html.dist));
+	return src(paths.html.src)
+		.pipe(
+			plumber({
+				//エラーがあっても処理を止めない
+				errorHandler: notify.onError("Error: <%= error.message %>"),
+			})
+		)
+		.pipe(
+			htmlMin({
+				//HTMLの圧縮
+				removeComments: true, //コメントを削除
+				collapseWhitespace: true, //余白を詰める
+				preserveLineBreaks: true, //タグ間の改行を詰める
+				removeEmptyAttributes: false //空属性を削除しない
+			})
+		)
+		.pipe(dest(paths.html.dist));
 };
 
 // Sassコンパイル
 const sassCompile = () => {
-  return src(paths.styles.src, {
-      sourcemaps: true,
-    })
-    .pipe(
-      plumber({
-        errorHandler: notify.onError("Error: <%= error.message %>"),
-      })
-    )
-    .pipe(
-      sass({
-        outputStyle: "expanded",
-      }).on("error", sass.logError)
-    )
-    .pipe(
-      postCss([
-        autoprefixer({
-          // プロパティのインデントを整形しない
-          cascade: false,
-          // IE11のgrid対応
-          grid: "autoplace",
-        }),
-      ])
-    )
-    //メディアクエリをまとめる
-    .pipe(gcmq())
-    .pipe(
-      dest(paths.styles.dist, {
-        sourcemaps: "./map",
-      })
-    )
-    // 変更があったらリロードせずにCSSのみ更新
-    .pipe(browserSync.stream())
+	return src(paths.styles.src, {
+			sourcemaps: true,
+		})
+		.pipe(
+			plumber({
+				errorHandler: notify.onError("Error: <%= error.message %>"),
+			})
+		)
+		.pipe(
+			sass({
+				outputStyle: "expanded",
+			}).on("error", sass.logError)
+		)
+		.pipe(
+			postCss([
+				autoprefixer({
+					// プロパティのインデントを整形しない
+					cascade: false,
+					// IE11のgrid対応
+					grid: "autoplace",
+				}),
+			])
+		)
+		//メディアクエリをまとめる
+		.pipe(gcmq())
+		.pipe(
+			dest(paths.styles.dist, {
+				sourcemaps: "./map",
+			})
+		)
+		// 変更があったらリロードせずにCSSのみ更新
+		.pipe(browserSync.stream())
 };
 
 // JavaScriptコンパイル
 const jsBabel = () => {
-  return src(paths.scripts.src)
-    .pipe(
-      plumber({
-        errorHandler: notify.onError("Error: <%= error.message %>"),
-      })
-    )
-    .pipe(
-      // Babel変換
-      babel({
-        presets: ["@babel/preset-env"],
-      })
-    )
-    .pipe(dest(paths.scripts.dist))
-    // JS圧縮
-    .pipe(uglify())
-    .pipe(dest(paths.scripts.dist));
+	return src(paths.scripts.src)
+		.pipe(
+			plumber({
+				errorHandler: notify.onError("Error: <%= error.message %>"),
+			})
+		)
+		.pipe(
+			// Babel変換
+			babel({
+				presets: ["@babel/preset-env"],
+			})
+		)
+		.pipe(dest(paths.scripts.dist))
+		// JS圧縮
+		.pipe(uglify())
+		.pipe(dest(paths.scripts.dist));
 };
 
 // 画像圧縮
 const imagesCompress = () => {
-  return src(paths.images.src, {
-      // 更新があった場合に処理
-      since: lastRun(imagesCompress),
-    })
-    .pipe(
-      plumber({
-        errorHandler: notify.onError("Error: <%= error.message %>"),
-      })
-    )
-    .pipe(
-      imagemin(
-        [
-          // JPG
-          imageminMozjpeg({
-            quality: 80,
-          }),
-          // PNG
-          imageminPngquant(
-            [0.7, 0.8] //画質の最小,最大
-          ),
-          // GIF
-          imageminGifsicle(),
-          // SVG
-          imageminSvgo({
-            plugins: [{
-                //フォトショやイラレで書きだされるviewboxを消さない
-                removeViewbox: false,
-              },
-              {
-                // メタデータを削除しない
-                removeMetadata: false,
-              },
-              {
-                // 不明な要素や属性を削除しない
-                removeUnknownsAndDefaults: false,
-              },
-              {
-                // <path>に変換しない
-                convertShapeToPath: false,
-              }
-            ],
-          }),
-        ], {
-          //ターミナルへの情報出力非表示
-          verbose: true,
-        }
-      )
-    )
-    .pipe(dest(paths.images.dist));
+	return src(paths.images.src, {
+			// 更新があった場合に処理
+			since: lastRun(imagesCompress),
+		})
+		.pipe(
+			plumber({
+				errorHandler: notify.onError("Error: <%= error.message %>"),
+			})
+		)
+		.pipe(
+			imagemin(
+				[
+					// JPG
+					imageminMozjpeg({
+						quality: 80,
+					}),
+					// PNG
+					imageminPngquant(
+						[0.7, 0.8] //画質の最小,最大
+					),
+					// GIF
+					imageminGifsicle(),
+					// SVG
+					imageminSvgo({
+						plugins: [{
+								//フォトショやイラレで書きだされるviewboxを消さない
+								removeViewbox: false,
+							},
+							{
+								// メタデータを削除しない
+								removeMetadata: false,
+							},
+							{
+								// 不明な要素や属性を削除しない
+								removeUnknownsAndDefaults: false,
+							},
+							{
+								// <path>に変換しない
+								convertShapeToPath: false,
+							}
+						],
+					}),
+				], {
+					//ターミナルへの情報出力非表示
+					verbose: true,
+				}
+			)
+		)
+		.pipe(dest(paths.images.dist));
 };
 
 // ローカルサーバー起動
 const browserSyncFunc = (done) => {
-  browserSync.init({
-    //デフォルトのconnectedのメッセージ非表示
-    notify: false,
-    server: {
-      baseDir: "./",
-    },
-    startPath: "./dist/index.html",
-    reloadOnRestart: true,
-  });
-  done();
+	browserSync.init({
+		//デフォルトのconnectedのメッセージ非表示
+		notify: false,
+		server: {
+			baseDir: "./",
+		},
+		startPath: "./dist/index.html",
+		reloadOnRestart: true,
+	});
+	done();
 };
 
 // ブラウザ自動リロード
 const browserReloadFunc = (done) => {
-  browserSync.reload();
-  done();
+	browserSync.reload();
+	done();
 };
 
 // ファイル監視
 const watchFiles = () => {
-  watch(paths.html.src, series(htmlFormat, browserReloadFunc));
-  watch(paths.styles.src, series(sassCompile));
-  watch(paths.scripts.src, series(jsBabel, browserReloadFunc));
-  watch(paths.images.src, series(imagesCompress, browserReloadFunc));
+	watch(paths.html.src, series(htmlFormat, browserReloadFunc));
+	watch(paths.styles.src, series(sassCompile));
+	watch(paths.scripts.src, series(jsBabel, browserReloadFunc));
+	watch(paths.images.src, series(imagesCompress, browserReloadFunc));
 };
 
 // npx gulp実行処理
 exports.default = series(
-  parallel(htmlFormat, sassCompile, jsBabel, imagesCompress),
-  parallel(watchFiles, browserSyncFunc)
+	parallel(htmlFormat, sassCompile, jsBabel, imagesCompress),
+	parallel(watchFiles, browserSyncFunc)
 );
